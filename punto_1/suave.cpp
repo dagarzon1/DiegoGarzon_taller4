01d731bed4a4a179a54ae6cf5c53bf9ce4bd9e2a
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-03-28 00:32:29 -0500

    suave

--- punto_1/suave.cpp
+++ punto_1/suave.cpp
@@ -1,1 +1,39 @@
-hola
+#include <iostream>
+#include <png.h>
+#include <stdlib.h>
+#include <zlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <tgmath.h>
+#include <math.h>
+#include <complex>
+
+
+using std::cout;
+using std::endl;
+
+void FT(double y[], double trans[]);
+int main()
+{
+cout<<"Hola"<<endl;
+return 0;
+}
+void FT(double* y, double* trans)
+{
+	double fun=*y;
+	int n= (sizeof(y)/sizeof(y[0]));
+	std::complex<double> res[n];
+	std::complex<double> com=(0.0,-2.0);
+	double resp[n];
+	double pi = std::acos(-1);
+	for(int i=0;i<n;i++)
+	{	
+		for(int j=0;j<n;j++)
+		{
+			std::complex<double> fo = ( com * pi * (double)i * (double)j ) / (double)n;
+			res[i] += y[j]* exp(fo);
+		}
+		resp[i]=std::abs(res[i]);
+	}
+	*trans = resp;	
+}
