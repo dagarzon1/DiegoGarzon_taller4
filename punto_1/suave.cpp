fefff7deecb5abe98f3c5f1e4b635006a62c9313
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-03-29 14:15:51 -0500

    imagen

--- punto_1/suave.cpp
+++ punto_1/suave.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <string.h>
 #include <math.h>
 #include <complex>
 #include <vector>
 #include <png.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace std;
 
-vector<vector<double> > FT(vector< vector <double> >& y, int inverse);
+vector< vector<double> > FT(vector< vector <double> >& y, int inverse);
+vector< vector<int> > readimage(char *filename);
 int main()
 {
 
 return 0;
 }
-vector<vector<double> > FT(vector< vector<double> >& y, int inverse)
+vector< vector<double> > FT(vector< vector<double> >& y, int inverse)
 {
 	int M= y.size();
 	int N= y[0].size();
 	vector< vector< complex<double> > > res(M, vector< complex<double> >(N,0) );
 	vector< vector<double> > trans(M, vector < double > (N,0) );
-	double inv=1.0;
+	double inv=-1.0;
 	if (inverse==1)
 	{
-		inv=-1.0;
+		inv=1.0;
 	}
 	double com=inv*2.0;
 	double pi = acos(-1);
@@ -43,3 +43,32 @@ vector<vector<double> > FT(vector< vector<double> >& y, int inverse)
 	}
 	return trans;
 }
+vector< vector<int> > read_image(char *filename)
+{	
+	FILE * f;
+	int bit, color, bytes;
+	png_uint_32 w,h;
+	png_structp ptr;
+	png_infop info;
+	png_bytepp filas;
+	f= fopen(filename,"r");
+	ptr=png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	info=png_create_info_struct(ptr);
+	png_init_io(ptr,f);
+	png_read_png(ptr,info,0,0);
+	png_get_IHDR(ptr,info, &w, &h, &bit, &color, NULL, NULL, NULL);
+	filas=png_get_rows(ptr, info);
+	bytes=png_get_rowbytes(ptr,info);
+	vector< vector<int> > img(h, vector < int > (w,0) );
+	for(int i=0;i<h;i++)
+	{
+		for(int j=0;j<w;j++)
+		{
+			img[i][j]=filas[i][j];
+		}
+	}
+	fclose(f);
+	return img;
+	
+}
+
