cdb7797c4fa85385cbfac257cbb3984ab976add6
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-03-28 23:35:17 -0500

    Fourier 2D

--- punto_1/suave.cpp
+++ punto_1/suave.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
-#include <png.h>
-#include <stdlib.h>
-#include <zlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <tgmath.h>
 #include <math.h>
 #include <complex>
+#include <vector>
+#include <png.h>
 
 
-using std::cout;
-using std::endl;
+using namespace std;
 
-void FT(double y[], double trans[]);
+vector<vector<double> > FT(vector< vector <double> >& y, int inverse);
 int main()
 {
-cout<<"Hola"<<endl;
+
 return 0;
 }
-void FT(double* y, double* trans)
+vector<vector<double> > FT(vector< vector<double> >& y, int inverse)
 {
-	double fun=*y;
-	int n= (sizeof(y)/sizeof(y[0]));
-	std::complex<double> res[n];
-	std::complex<double> com=(0.0,-2.0);
-	double resp[n];
-	double pi = std::acos(-1);
-	for(int i=0;i<n;i++)
-	{	
-		for(int j=0;j<n;j++)
-		{
-			std::complex<double> fo = ( com * pi * (double)i * (double)j ) / (double)n;
-			res[i] += y[j]* exp(fo);
-		}
-		resp[i]=std::abs(res[i]);
+	int M= y.size();
+	int N= y[0].size();
+	vector< vector< complex<double> > > res(M, vector< complex<double> >(N,0) );
+	vector< vector<double> > trans(M, vector < double > (N,0) );
+	double inv=1.0;
+	if (inverse==1)
+	{
+		inv=-1.0;
 	}
-	*trans = resp;	
+	double com=inv*2.0;
+	double pi = acos(-1);
+	int k,l;
+	for(k=0,l=0;k<M || l<N;k++,l++)
+	{	
+			complex<double> t=(0.0,0.0);
+			int n,m;
+			for(m=0,n=0;m<M || n<N;n++,m++)
+			{
+				double fo = com * pi * ((m*k/M)+(n*l/N));
+				t = t + (y[m][n] * polar(1.0,fo));
+			}
+			res[k][l]=t;
+			double r=abs(res[k][l] / ((double)N *(double)M ) );
+			trans[k][l]=r;
+	}
+	return trans;
 }
